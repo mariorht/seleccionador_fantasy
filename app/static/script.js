dddf0df7b92365fd6d0a7ae4d26241988d1f1234
mario <marioroos2502@gmail.com>
2024-05-19 18:11:01 +0200

    Se empiezan a poder seleccionar jugadores

--- app/static/script.js
+++ app/static/script.js
@@ -10,6 +10,29 @@ document.addEventListener("DOMContentLoaded", function() {
 
 $(document).ready(function() {
     $('#playerTable').DataTable({
-        "order": [[ 3, 'desc' ]] // Ordenar por la tercera columna (Rating) en orden descendente
+        "order": [[ 3, "desc" ]] // Ordenar por la tercera columna (Rating) en orden descendente
+    });
+
+   
+    // Añadir evento de clic a cada jugador en el campo
+    $('.player').click(function() {
+        if ($(this).find('.player-select').length === 0) {
+            const position = $(this).data('position');
+            let select = `<select class="player-select" data-position="${position}">
+                            <option value="">Select player</option>`;
+            playersData.forEach(player => {
+                select += `<option value="${player['Player Name']}">${player['Player Name'].replace(/-/g, ' ')}</option>`;
+            });
+            select += `</select>`;
+            $(this).html(select);
+
+            // Añadir evento de cambio al select
+            $('.player-select').change(function() {
+                const selectedPlayer = $(this).val();
+                const position = $(this).data('position');
+                $(`.player[data-position="${position}"]`).html(selectedPlayer);
+            });
+        }
     });
 });
+
